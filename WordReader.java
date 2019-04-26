52dcdf4ec729cca754cfdf87752ada88b72f2578
Julia <44108941+Julia352@users.noreply.github.com>
2019-04-26 13:04:48 -0400

    Update WordReader.java

--- WordReader.java
+++ WordReader.java
@@ -1,35 +1,43 @@
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class WordReader {
-    private static final String fileName = "/res/words.txt";
 
-    private ArrayList<String> words = new ArrayList<String>();
-    private int i = -1;
+	private ArrayList<String[]> words = new ArrayList<String[]>();
+	private ArrayList<String[]> wordsCopy = new ArrayList<String[]>();
 
-    public WordReader(String fileName) {
-    	 
-        try (Scanner input = new Scanner(Paths.get(fileName))) {
-               
+	public WordReader(String wordsFileName, String questionsFileName) {
 
-            
-            while (input.hasNext())
-            	
-                words.add(    input.next()  );
-        }
-        catch (Exception e) {
-            System.out.println("Couldn't find/read file: " + fileName);
-            System.out.println("Error message: " + e.getMessage());
-        }
-    }
+		try (Scanner wordsInput = new Scanner(Paths.get(wordsFileName))) {
+			try (Scanner questionsInput = new Scanner(Paths.get(questionsFileName))) {
 
-   public String getRandomWord() {
-    	i++;
-    	return words.get(i);
-    	
-    }
-        
+				while (wordsInput.hasNext()) {
+					words.add(new String[2]);
+					words.get(words.size() - 1)[0] = wordsInput.next();
+					words.get(words.size() - 1)[1] = questionsInput.next();
+				}
+				
+			} catch (Exception e) {
+				System.out.println("Couldn't find/read file: " + questionsFileName);
+				System.out.println("Error message: " + e.getMessage());
+			}
+
+		} catch (Exception e) {
+			System.out.println("Couldn't find/read file: " + wordsFileName);
+			System.out.println("Error message: " + e.getMessage());
+		}
+		
+		wordsCopy.addAll(words);
+
+	}
+
+	public String[] getRandomWord() {
+		int i = (int) (Math.random() * wordsCopy.size());
+		String[] result = wordsCopy.get(i);
+		wordsCopy.remove(i);
+		return result;
+	}
+
 }
